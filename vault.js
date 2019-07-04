cd5be0fb752593f052192bc58b15144781ed8c86
Andrew Obiano <andrew.obiano@gmail.com>
2019-07-03 18:35:45 -1000

    finished

--- vault.js
+++ vault.js
@@ -1,4 +1,19 @@
 'use strict';
 module.exports = function() {
-  
+    let vaultKey = '';
+    let newValue = '';
+    return{
+        getValue: function(key){
+            if(!key || key !== vaultKey){
+                return null;
+            }else{
+                return newValue;
+            }
+        },
+        setValue: function(key, value){
+            vaultKey = key;
+            newValue = value;
+        }
+    }
+
 };
