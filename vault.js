206e5be4026d56f8851f66d794b618e12c668128
Andrew Obiano <andrew.obiano@gmail.com>
2020-01-04 16:06:27 -1000

    made corrections to final answer

--- vault.js
+++ vault.js
@@ -1,19 +1,18 @@
-'use strict';
+"use strict";
 module.exports = function() {
-    let vaultKey = '';
-    let newValue = '';
-    return{
-        getValue: function(key){
-            if(!key || key !== vaultKey){
-                return null;
-            }else{
-                return newValue;
-            }
-        },
-        setValue: function(key, value){
-            vaultKey = key;
-            newValue = value;
-        }
+  let vaultKey = {};
+  let newValue = "";
+  return {
+    getValue: function(key) {
+      if (!key || key !== vaultKey[key]) {
+        return null;
+      } else {
+        return newValue;
+      }
+    },
+    setValue: function(key, value) {
+      vaultKey[key] = key;
+      newValue = value;
     }
-
+  };
 };
